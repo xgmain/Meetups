f89a1b406202381fe343952f824ae10afc2ab915
Nix <xin.gao.au@outlook.com>
2026-08-09 16:49:52 +1000

    fix map issue

--- client/src/features/meetups/details/MeetupDetailsInfo.tsx
+++ client/src/features/meetups/details/MeetupDetailsInfo.tsx
@@ -1,12 +1,15 @@
 ﻿import { CalendarToday, Info, Place } from "@mui/icons-material";
-import { Divider, Grid, Paper, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid, Paper, Typography } from "@mui/material";
 import { formatDate } from "../../../lib/util/util";
+import { useState } from "react";
+import MapComponent from "../../../app/shared/components/MapComponent";
 
 type Props = {
     meetup: Meetup
 }
 
 export default function MeetupInfo({meetup}: Props) {
+    const [mapOpen, setMapOpen] = useState(false);
     return (
         <Paper sx={{ mb: 2 }}>
 
@@ -33,12 +36,24 @@ export default function MeetupInfo({meetup}: Props) {
                 <Grid size={1}>
                     <Place color="info" fontSize="large" />
                 </Grid>
-                <Grid size={11}>
+                <Grid size={11} display='flex' justifyContent='space-between' alignItems='center'>
                     <Typography>
                         {meetup.venue}, {meetup.city}
                     </Typography>
+                    <Button onClick={() => setMapOpen(!mapOpen)}>
+                        {mapOpen ? 'Hide Map' : 'Show Map'}
+                    </Button>
                 </Grid>
             </Grid>
+            {mapOpen && (
+                <Box sx={{height: 400, zIndex: 1000, display: 'block'}}>
+                    <MapComponent
+                        position={[meetup.latitude, meetup.longitude]}
+                        venue={meetup.venue}
+                    />
+                </Box>
+
+            )}
         </Paper>
     )
 }
